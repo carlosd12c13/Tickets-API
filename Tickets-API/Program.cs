7fca93c767ec2c420b8522aba90627421ffb5fee
carlosd12c13 <58006644+carlosd12c13@users.noreply.github.com>
2026-05-31 16:33:09 -0600

    chore(dev): add sample users seed data

--- Tickets-API/Program.cs
+++ Tickets-API/Program.cs
@@ -16,6 +16,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await DbInitializer.SeedAsync(dbContext);
+}
+
 // Configure the HTTP request pipeline.
 
 if (app.Environment.IsDevelopment())
