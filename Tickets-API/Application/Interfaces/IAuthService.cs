cbea71c3d0bfe3fb2d859f69a61b934fb75e5353
carlosd12c13 <58006644+carlosd12c13@users.noreply.github.com>
2026-05-31 18:36:52 -0600

    feat(auth): add login service and JWT token generation

---
+++ Tickets-API/Application/Interfaces/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿using Tickets_API.Application.DTOs.Auth;
+
+namespace Tickets_API.Application.Interfaces;
+
+public interface IAuthService
+{
+    Task<LoginResponse?> LoginAsync(LoginRequest request);
+}
---
+++ Tickets-API/Application/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,8 @@
+﻿using Tickets_API.Domain.Entities;
+
+namespace Tickets_API.Application.Interfaces;
+
+public interface IJwtTokenService
+{
+    string CreateToken(User user, DateTime expiresAt);
+}
